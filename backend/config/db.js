0a2149ba41f1911cb7f581798dee26f2dbe79cac
Shashank Kumar Singh <182289608+shashank-4bt@users.noreply.github.com>
2026-05-11 01:40:15 +0530

    backend

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async() =>{
+    const DB_NAME = 'SkillynDB'
+    const DB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jspv37k.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+    
+    try{
+        const connectionInstance = await mongoose.connect(DB_URI)
+        console.log(`\nMONGO DB CONNECTED !!`);
+
+    }catch(err){
+        console.log(`MONGODB CONNECTION ERROR: ${err}`);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
